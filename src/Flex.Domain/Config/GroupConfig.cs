95fa7d37849f2a15000370d7838b20c63b78bbac
latiao195 <2669701901@qq.com>
2023-12-11 14:41:51 +0800

    菜单表默认字段长度修改

--- src/Flex.Domain/Config/GroupConfig.cs
+++ src/Flex.Domain/Config/GroupConfig.cs
@@ -9,7 +9,8 @@ namespace Flex.Domain.Config
         public void Configure(EntityTypeBuilder<SysGroup> builder)
         {
             base.ConfigureLongId(builder);
-
+            builder.Property(m => m.GroupName).IsRequired().HasMaxLength(50);
+            builder.Property(m => m.MenuPermissions).HasMaxLength(2000);
             builder.ToTable("tbl_core_group");
         }
     }
