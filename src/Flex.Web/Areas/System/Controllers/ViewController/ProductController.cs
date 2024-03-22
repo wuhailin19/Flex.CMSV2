6c48fe32aa8120283f02a772f163988d1fb0290e
latiao195 <2669701901@qq.com>
2024-03-22 17:12:04 +0800

    修改接口授权部分

---
+++ src/Flex.Web/Areas/System/Controllers/ViewController/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Flex.Core.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flex.Web.Areas.system.Controllers.ViewController
+{
+    [Area("System")]
+    [Descriper(Name = "项目管理页面")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Flex.Web/wwwroot/system/js/productmg/index.js
@@ -0,0 +1,12 @@
+﻿$('.middle-link a').off('click').on('click',function (event) {
+    //event.preventDefault();
+    $(this).addClass('active').siblings().removeClass('active');
+    //var hash = $(this).attr("href").split("#")[1];
+    //animateScroll("#"+hash);
+})
+function animateScroll($elment) {
+    var itop = $($elment).offset().top;
+    $(".content-right-box").stop().animate({
+        scrollTop: itop
+    }, 300);
+}
